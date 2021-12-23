4f0c573677362b8dfb4b3a71337ec409958cd980
Justin Williams <justin@ameronix.com>
2021-12-23 14:23:43 -0500

    fetched posts from database and added some basic tailwind to Post list

--- resources/js/PostList.vue
+++ resources/js/PostList.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
-
-        Post List
+    <div class="container mx-auto px-4 w-full md:w-3/4 lg:w-3/5 xl:w-1/2 my-20">
+        <h2 class="text-4xl">All Posts</h2>
+        <div v-if="$apollo.loading">Loading</div>
+        <div v-else>
+            <div v-for="post in posts" :key="post.id">
+                {{ post.id }} is titled {{ post.title }}
+            </div>
+        </div>
     </div>
 </template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+    apollo: {
+        posts: gql`
+            {
+                posts {
+                    id
+                    title
+                }
+            }
+        `,
+    },
+};
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import PostList from './PostList';
 import Post from './Post'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
 
 Vue.use(VueRouter);
 
@@ -23,6 +25,15 @@ const routes=[
 
 
 ]
+Vue.use(VueApollo);
+const apolloClient = new ApolloClient({
+  // You should use an absolute URL here
+  uri: 'http://127.0.0.1:8000/graphql',
+
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 const router = new VueRouter({
 mode:'history',
   routes
@@ -30,5 +41,6 @@ mode:'history',
 
 const app = new Vue({
   el: '#app',
+  apolloProvider,
   router
 });
