540af394f9bf5705b4264ad0cbec39800d820d3e
Justin Williams <justin@ameronix.com>
2021-12-23 12:13:33 -0500

    added Vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
+import './bootstrap';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(VueRouter);
 const app = new Vue({
   el: '#app',
 });
